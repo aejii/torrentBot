63f8268c628ef459f2ed186ab13c23f747db790e
hmmhmmhm <hmmhmmhm@naver.com>
2019-12-28 22:52:16 +0900

    Update

--- src/example.ts
+++ src/example.ts
@@ -1,12 +1,12 @@
-import { init, saftyGoto } from './utils/puppeteer'
+import { init, saftyGoto, delay } from './utils/puppeteer'
 import { Browser } from 'puppeteer'
 
-export const exampleSequence = async () => {
-    let browser = await init()
-    if(browser){
+export const crawlingSequence = async () => {
+    let browser = await init({ isHeadless: true })
+    if (browser) {
         console.log('Crawling...')
         await crawling(browser)
-    }else{
+    } else {
         console.log('헤드리스 크롬을 켜는데 실패하였습니다.')
     }
 }
@@ -20,14 +20,75 @@ export const crawling = async (browser: Browser) => {
     await saftyGoto(page, 'https://torrentwal2.com/')
     console.log('토렌트 사이트로 접속 되었습니다.')
 
-    let bodyInnerHTML = await page.evaluate(()=>{
+    let bodyInnerHTML = await page.evaluate(() => {
+        // 아래 코드 같은거로 원하는 정보만 쏙 빼올 수 있습니다.
+        // document.querySelector('원하는 HTML 셀렉터')
 
         // body 전체를 그냥 긁어옵니다.
         return document.body.innerHTML
     })
 
     console.log('bodyInnerHTML 길이:', bodyInnerHTML.length)
-    console.log(bodyInnerHTML)
 }
 
-exampleSequence()
+export const accessSequence = async () => {
+    let browser = await init({ isHeadless: false })
+    if (browser) {
+        console.log('Accessing...')
+        await accessing(browser)
+    } else {
+        console.log('헤드리스 크롬을 켜는데 실패하였습니다.')
+    }
+}
+
+export const accessing = async (browser: Browser) => {
+    let page = await browser.newPage()
+
+    console.log('NAS 사이트로 접속을 시도합니다.')
+
+    // 원하는 페이지로 이동합니다.
+    await saftyGoto(
+        page,
+        'http://saint2030.synology.me:5000/',
+        'domcontentloaded'
+    )
+    console.log('NAS 사이트로 접속 되었습니다.')
+
+    // 이미 로그인 된 경우는
+    // 아이디 입력칸이 없어지는데,
+    // 오류를 try ~ catch 함으로 인해 알아서 다음으로 넘어감
+    try{
+        // 3초간 혹시 모르니 대기합니다.
+        await delay(3000)
+        await page.type('#login_username', 'test', { delay: 200 })
+        console.log('아이디 입력완료')
+        
+        await page.type('#login_passwd', 'qudwns12!@', { delay: 200 })
+        console.log('비밀번호 입력완료')
+    
+        // 버튼 누르기 전 혹시 모르니 1초간 기다립니다.
+        await delay(1000)
+        console.log('로그인 시도 중..')
+        await page.click('#login-btn')
+
+        // 대략 4초간 기다립니다. (SPA라 페이지 이동 파악 불가)
+        await delay(4000)
+        console.log('로그인 완료')
+
+    }catch(e){}
+
+    //await page.click('.launch-icon')
+    //console.log('파일 스테이션 버튼 클릭 완료')
+
+    await page.click('[aria-label="메인 메뉴"]')
+    console.log('메인 메뉴 버튼 클릭 완료')
+
+    // 혹시 모르니 2초 대기
+    await delay(2000)
+
+    await page.click('[aria-label="Download Station"]')
+    console.log('Download Station 버튼 클릭 완료')
+}
+
+// crawlingSequence()
+accessSequence()
--- src/utils/puppeteer.ts
+++ src/utils/puppeteer.ts
@@ -2,9 +2,9 @@ import puppeteer from 'puppeteer'
 import puppeteerExtra from 'puppeteer-extra'
 import pluginStealth from 'puppeteer-extra-plugin-stealth'
 import cloudscraper from 'cloudscraper'
-let scraper:any = cloudscraper
+let scraper: any = cloudscraper
 
-export const init = async () => {
+export const init = async ({ isHeadless = true }) => {
     console.log(`🚧  초기 실행 진행 중...`)
     console.log(`🚧  헤드리스 크롬을 시작하고 있습니다..`)
     console.log(`🚧  중간에 언제든 Ctrl+C로 종료시킬 수 있습니다.\n`)
@@ -22,7 +22,7 @@ export const init = async () => {
 
         const options = {
             args,
-            headless: true,
+            headless: isHeadless,
             ignoreHTTPSErrors: true,
             userDataDir: './tmp',
         }
@@ -32,7 +32,7 @@ export const init = async () => {
 
         let browser = await puppeteerExtra.launch(options)
         console.log(`🚧  헤드리스 크롬이 시작되었습니다.`)
-        
+
         return browser
     } catch (e) {
         console.log(`🚧  헤드리스 크롬 작동 중 오류가 발생하였습니다.`)
@@ -43,9 +43,13 @@ export const init = async () => {
 }
 
 // 페이지 이동
-export const navigatePage = async (page: puppeteer.Page, targetUrl: string) => {
+export const navigatePage = async (
+    page: puppeteer.Page,
+    targetUrl: string,
+    waitCode: string = 'networkidle0'
+) => {
     try {
-        let hookHeaders: any = await scrapeCloudflareHttpHeaderCookie (targetUrl)
+        let hookHeaders: any = await scrapeCloudflareHttpHeaderCookie(targetUrl)
 
         // Anti Cloud Flare
         await page.setRequestInterception(true)
@@ -54,8 +58,9 @@ export const navigatePage = async (page: puppeteer.Page, targetUrl: string) => {
             request.continue({ ...hookHeaders })
         })
 
+        // @ts-ignore
         await page.goto(targetUrl, {
-            waitUntil: ['load', 'networkidle0'],
+            waitUntil: ['load', waitCode],
         })
 
         return true
@@ -69,22 +74,26 @@ export const navigatePage = async (page: puppeteer.Page, targetUrl: string) => {
 }
 
 // 페이지 안전 이동
-export const saftyGoto = async (page: puppeteer.Page, targetUrl: string) => {
+export const saftyGoto = async (
+    page: puppeteer.Page,
+    targetUrl: string,
+    waitCode: string = 'networkidle0'
+) => {
     // 성공할때까지 계속 접속을 시도합니다.
     let isSuccess = false
-    while (!isSuccess) isSuccess = await navigatePage(page, targetUrl)
+    while (!isSuccess) isSuccess = await navigatePage(page, targetUrl, waitCode)
 }
 
 export const delay = time => new Promise(res => setTimeout(res, time))
 
 // 스크레이퍼 초기화
-export const scrapeCloudflareHttpHeaderCookie = (url) =>
-	(new Promise ((resolve, reject) =>
-		(scraper.get (url, function (error, response, body) {
-			if (error) {
-				reject (error)
-			} else {
-				resolve (response.request.headers)
-			}
-		}))
-    ))
+export const scrapeCloudflareHttpHeaderCookie = url =>
+    new Promise((resolve, reject) =>
+        scraper.get(url, function(error, response, body) {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(response.request.headers)
+            }
+        })
+    )
