160bfcfdc2fea1265f5e769b33ed836bf55fe5ef
hmmhmmhm <hmmhmmhm@naver.com>
2019-12-28 21:56:17 +0900

    Apply Typescript

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import TelegramBot from 'node-telegram-bot-api'
+
+const token = process.env.TELEGRAM_KEY
+const bot = new TelegramBot(token, { polling: true })
+
+bot.on('message', async msg => {
+    const chatId = msg.chat.id
+    const Hi = 'hi'
+    if (
+        msg.text
+            .toString()
+            .toLowerCase()
+            .indexOf(Hi) === 0
+    ) {
+        await bot.sendMessage(msg.chat.id, `${chatId}하이하이`)
+    }
+})
+
+bot.on('polling_error', error => {
+    console.log(error.code) // => 'EFATAL'
+})
